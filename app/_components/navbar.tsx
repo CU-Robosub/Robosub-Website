6466c5afd71f5d878337a9f218733c5941942893
William Hardee <williamhardee6@gmail.com>
2025-03-12 18:40:13 -0600

    technology is responsive

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -69,9 +69,8 @@ export const Navbar = () => {
           <div className="flex-row items-center text-center cursor-pointer  py-1 w-2/5 bg-blue-500 dark:bg-blue-900 hover:bg-blue-400 duration-150 rounded-xl">
             Join
           </div>
-          <div onClick={themeChange} className="flex-row w-2/5 items-center cursor-pointer py-1 px-2 dark:bg-yellow-500 dark:hover:bg-yellow-600 bg-blue-800  hover:bg-blue-950 duration-150 rounded-xl">
-            <Image className="hidden dark:block mx-auto" src="/svg/sun.svg" width={25} height={25} alt="sun" />
-            <Image className="dark:hidden mx-auto" src="/svg/moon.svg" width={25} height={25} alt="sun" />
+          <div className="w-2/5">
+            <ThemeSwitcher/>
           </div>
         </div>
         <div onClick={() => {setOpen(!open)}} className={`duration-150 cursor-pointer relative top-1 left-1 w-10 h-10 border-gray-400 dark:bg-gray-900 bg-white border-2 rounded-md ${open ? "-translate-x-44" : ""}`}>
--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -52,16 +52,18 @@ export default function Technology() {
 
     return (
         <>
-            <div className="relative w-full h-full grid grid-rows-10 grid-cols-10">
-                <div className="col-span-5 row-span-10 bg-gradient-to-b from-blue-500 via-blue-600 to-blue-700 dark:from-blue-700 dark:via-blue-800 dark:to-blue-900">
+            <div className="relative w-full h-auto min-h-[100vh] flex flex-col md:flex-row">
+                {/* Left Section */}
+                <div className="w-full md:w-1/2 h-auto bg-gradient-to-b from-blue-500 via-blue-600 to-blue-700 dark:from-blue-700 dark:via-blue-800 dark:to-blue-900">
+                    <div className="flex flex-col w-full mx-auto py-20 px-6 md:px-20 items-center justify-around relative gap-6">
 
-                    <div className="flex flex-col w-full mx-auto py-20 px-20 items-center justify-around relative ">
-                        <div className="shadow-md overflow-hidden text-lg md:text-3xl   flex flex-wrap md:flex-nowrap rounded-full border-2 justify-around items-center border-black dark:border-white">
+                        {/* Button Section */}
+                        <div className="shadow-md overflow-hidden text-lg md:text-2xl lg:text-3xl flex md:flex-nowrap rounded-full border-2 justify-around items-center border-black dark:border-white">
                             {["chimera", "lazarus", "leviathan"].map((sub) => (
                                 <div
                                     key={sub}
                                     onClick={() => setSelectedSub(sub)}
-                                    className=" hover:bg-blue-600 duration-100 cursor-pointer w-full md:w-auto flex flex-row items-center justify-center px-4 py-2 md:h-full"
+                                    className="hover:bg-blue-600 duration-100 cursor-pointer w-full md:w-auto flex flex-row items-center justify-center px-4 py-2 md:h-full"
                                 >
                                     {sub.charAt(0).toUpperCase() + sub.slice(1)}
                                 </div>
@@ -69,16 +71,16 @@ export default function Technology() {
                         </div>
 
                         {/* Text Section */}
-                        <div className="flex flex-col w-full h-auto md:h-full mb-8 md:mb-0">
-                            <div className="shadow-lg bg-blue-600 dark:bg-blue-800 p-3 m-3 mt-6 h-[16rem]">
-                                <div className="text-3xl font-bold">Description.</div>
-                                <p className="text-lg md:text-xl my-3 text-center md:text-left">
+                        <div className="flex flex-col w-full h-auto gap-6">
+                            <div className="shadow-lg bg-blue-600 dark:bg-blue-800 p-4 m-3 mt-6 h-auto md:h-[16rem]">
+                                <div className="text-2xl md:text-3xl font-bold">Description.</div>
+                                <p className="text-base md:text-xl my-3 text-center md:text-left">
                                     {subInfo[selectedSub].description}
                                 </p>
                             </div>
-                            <div className="shadow-lg h-auto md:h-40 flex flex-col p-2 space-y-4 overflow-hidden">
-                                <h1 className="text-xl font-bold">Specification.</h1>
-                                <div className="text-xl">
+                            <div className="shadow-lg h-auto md:h-40 flex flex-col p-4 space-y-4 overflow-hidden">
+                                <h1 className="text-lg md:text-xl font-bold">Specification.</h1>
+                                <div className="text-lg md:text-xl">
                                     {stats.map((stat) => (
                                         <StatItem
                                             key={`${selectedSub}-${stat.icon}`}
@@ -93,14 +95,25 @@ export default function Technology() {
                         </div>
                     </div>
                 </div>
-                <div className="rounded-lg relative col-span-5 bg-slate-50 dark:bg-gray-800 row-span-8 col-start-6 row-start-2 shadow-lg m-10 overflow-hidden flex flex-col justify-center items-center">
-                    <Image className="z-10" src={`${subInfo[selectedSub].img}`} width={450} height={450} alt={`${selectedSub}`} />
-                    <div className="absolute top-1 left-1 text-5xl font-semibold">{selectedSub.toUpperCase()}</div>
-                    <div className="absolute w-full h-full top-0 left-0 overflow-hidden flex flex-col justify-center items-center"><WavyBackground/></div>
+
+                {/* Right Section */}
+                <div className="w-full md:w-1/2 h-auto flex flex-col justify-center items-center p-6">
+                    <div className="w-full max-w-md md:max-w-lg lg:max-w-xl h-[32rem] bg-slate-50 dark:bg-gray-800 shadow-lg rounded-lg relative overflow-hidden flex flex-col justify-center items-center p-4">
+                        <Image
+                            className="z-10 max-w-full h-auto"
+                            src={`${subInfo[selectedSub].img}`}
+                            width={450}
+                            height={450}
+                            alt={`${selectedSub}`}
+                        />
+                        <div className="absolute top-1 left-1 text-3xl md:text-5xl font-semibold">{selectedSub.toUpperCase()}</div>
+                        <div className="absolute w-full h-full top-0 left-0 overflow-hidden flex flex-col justify-center items-center">
+                            <WavyBackground />
+                        </div>
+                    </div>
                 </div>
-
-
             </div>
+
 
             <Footer />
         </>
