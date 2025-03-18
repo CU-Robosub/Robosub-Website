f77a531b1ab28bf18eb8c682158dbe1ac965b6f4
William Hardee <williamhardee6@gmail.com>
2025-03-17 18:42:39 -0600

    updated technology page with better descriptions

--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -20,21 +20,21 @@ interface SubInfo {
 const subInfo: Record<string, SubInfo> = {
     chimera: {
         img: "/subs/chimera.png",
-        description: "Our newest sub with 8 motors to allow for thrust vectoring and superior mobility. The modular design makes it easy to swap components in and out. Multidimensional gripper to allow for complex manipulation of objects",
+        description: "Chimera is a versatile submersible designed for superior controllability and autonomy. Equipped with 8 motors, a gripper rotation mechanism, and integrated lights, it excels in object manipulation and visibility in all conditions. Its modular design simplifies maintenance and component swapping, while the sliding electronics rack enhances accessibility. Chimera also features a Doppler Velocity Logger (DVL) for precise positioning and orientation, supported by software that uses sensor data to enable fully autonomous decision-making.",
         cost: 16500,
         hours: 2700,
         weight: 20,
     },
     lazarus: {
         img: "/subs/lazarus.png",
-        description: "Our newest sub with 8 motors to allow for thrust vectoring and superior mobility. The modular design makes it easy to swap components in and out. Multidimensional gripper to allow for complex manipulation of objects",
+        description: "Lazarus is a lightweight and nimble submersible designed for advanced task capabilities in both MATE ROV and RoboSub competitions. Outfitted with a Blue Robotics Newton Gripper and a custom camera housing, it is prepared to tackle complex challenges. With a compact frame (52cm x 50cm x 15cm), Lazarus is highly maneuverable and energy-efficient. Additionally, it features thruster guards made from 1/8 acrylic, ensuring safety and durability by meeting IP20 standards for debris protection.",
         cost: 12300,
         hours: 2220,
-        weight: 30,
+        weight: 20,
     },
     leviathan: {
         img: "/subs/leviathan.png",
-        description: "Our newest sub with 8 motors to allow for thrust vectoring and superior mobility. The modular design makes it easy to swap components in and out. Multidimensional gripper to allow for complex manipulation of objects",
+        description: "Leviathan achieves unmatched visual sensing and autonomy in the RoboSub competition with its nine 60 Hz cameras. To ensure rapid visual analysis, the team transitioned much of its vision software from Python to C++ with Nodeleting, significantly boosting processing speed. Powered by an Intel i7 processor and an NVIDIA GTX 1050 Ti, Leviathan delivers the computational strength needed to support its advanced autonomous systems, making it a standout in performance and capability.",
         cost: 12700,
         hours: 1200,
         weight: 15,
@@ -46,7 +46,7 @@ export default function Technology() {
 
     const stats = [
         { icon: "money", value: subInfo[selectedSub].cost, unit: "dollars", delay: 0.2 },
-        { icon: "dumbell", value: subInfo[selectedSub].weight, unit: "pounds", delay: 0.3 },
+        { icon: "dumbell", value: subInfo[selectedSub].weight, unit: "kg", delay: 0.3 },
         { icon: "clock", value: subInfo[selectedSub].hours, unit: "hours", delay: 0.4 }
     ];
 
@@ -72,9 +72,9 @@ export default function Technology() {
 
                         {/* Text Section */}
                         <div className="flex flex-col w-full h-auto gap-6">
-                            <div className="shadow-lg bg-blue-600 dark:bg-blue-800 p-4 m-3 mt-6 h-auto md:h-[16rem]">
+                            <div className="shadow-lg bg-blue-600 dark:bg-blue-800 p-4 m-3 mt-6 h-auto md:min-h-[22rem]">
                                 <div className="text-2xl md:text-3xl font-bold">Description.</div>
-                                <p className="text-base md:text-xl my-3 text-center md:text-left">
+                                <p className="text-base md:text-lg my-3 text-center md:text-left">
                                     {subInfo[selectedSub].description}
                                 </p>
                             </div>
