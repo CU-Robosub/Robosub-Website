b91cd51c1afcae283345dcc8052a62c60b6b79c4
William Hardee <williamhardee6@gmail.com>
2025-03-31 18:47:31 -0600

    fixed icon height

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
       {
         url: '/og-image.jpg',
         width: 1200,
-        height: 630,
+        height: 550,
         alt: 'Colorado Robosub Autonomous Underwater Vehicles'
       }
     ]
